14ae01af2e62973aca7656becaefea4a31d56b5f
KanchanBora <108566907+KanchanBora@users.noreply.github.com>
2023-07-13 15:42:39 +0530

    Update Kth largest element in the unsorted array.cpp

--- Day 22. Binary Trees[Miscellaneous]/Kth largest element in the unsorted array.cpp
+++ Day 22. Binary Trees[Miscellaneous]/Kth largest element in the unsorted array.cpp
@@ -2,6 +2,17 @@
 int kthLargest(vector<int>& arr, int size, int K)
 {
 	// Write your code here.
-    sort(arr.begin(),arr.end());
-    return arr[size-K];
+
+    /*sort(arr.begin(),arr.end());
+    return arr[size-K];*/
+
+    priority_queue<int, vector<int>, greater<int>>pq;
+    for(auto el:arr)
+    {
+        pq.push(el);
+        if(pq.size()> K){
+             pq.pop();
+        } 
+    }
+        return pq.top();
 }
