c8b93ecfd0ecfadb9a947c2ca5bc0879f92e4a8c
KanchanBora <108566907+KanchanBora@users.noreply.github.com>
2023-07-20 23:44:58 +0530

    Update Minimum Path Sum.cpp

--- Day 26. Dynamic Programming Part-II/Minimum Path Sum.cpp
+++ Day 26. Dynamic Programming Part-II/Minimum Path Sum.cpp
@@ -1,19 +1,34 @@
 #include <bits/stdc++.h> 
 int minSumPath(vector<vector<int>> &grid) {
-    int m = grid.size();
-    int n = grid[0].size(); 
+    int n = grid.size();
+    int m = grid[0].size(); 
 
-    vector<vector<int> > sum(m, vector<int>(n, grid[0][0]));
+    vector<vector<int> > dp(n, vector<int>(m, 0));
+    vector<int>prev(m,0);
+    for(int i=0;i<n; i++)
+    {
+        vector<int>curr(m,0);
+    for (int j = 0; j < m; j++)
+    {
+        if(i==0 && j==0 )
+            curr[j]= grid[i][j];
+        else {
+          int up = grid[i][j];
+          if (i > 0)
+            up += prev[j];
+          else
+            up += 1e9;
 
-    for (int i = 1; i < m; i++)
-        sum[i][0] = sum[i - 1][0] + grid[i][0];
+          int left = grid[i][j];
+          if (j > 0)
+            left += curr[j - 1];
+          else
+            left += 1e9;
 
-    for (int j = 1; j < n; j++)
-        sum[0][j] = sum[0][j - 1] + grid[0][j];
-
-    for (int i = 1; i < m; i++)
-        for (int j = 1; j < n; j++)
-            sum[i][j]  = min(sum[i - 1][j], sum[i][j - 1]) + grid[i][j];
-            
-    return sum[m - 1][n - 1];
+          curr[j] = min(left, up);
+        }
+    }
+    prev = curr;
+}
+return prev[m - 1];
 }
